f2b68a2e1b68251312540d346c9c49ac76eeccb2
Korbinian Robert Randl <kora8563@dsv.su.se>
2024-11-29 11:36:15 +0100

    Renamed experiment scripts.

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # Experiments for Gemma 2B:
-python experiment-hazard.py google/gemma-1.1-2b-it
-python experiment-movies.py google/gemma-1.1-2b-it
+python self-explanations-hazard.py google/gemma-1.1-2b-it
+python self-explanations-movies.py google/gemma-1.1-2b-it
 
 # Experiments for Gemma 7B:
-python experiment-hazard.py google/gemma-1.1-7b-it
-python experiment-movies.py google/gemma-1.1-7b-it
+python self-explanations-hazard.py google/gemma-1.1-7b-it
+python self-explanations-movies.py google/gemma-1.1-7b-it
 
 # Experiments for Llama 3.1 8B:
-python experiment-hazard.py meta-llama/Meta-Llama-3.1-8B-Instruct
-python experiment-movies.py meta-llama/Meta-Llama-3.1-8B-Instruct
+python self-explanations-hazard.py meta-llama/Meta-Llama-3.1-8B-Instruct
+python self-explanations-movies.py meta-llama/Meta-Llama-3.1-8B-Instruct
